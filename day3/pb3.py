9ccd77e7c34363a7e717f8be8b0074def54e30c8
TT54 <tt54.of@gmail.com>
2025-12-03 09:39:27 +0100

    Résolution problème 2 jour 3

--- day3/pb3.py
+++ day3/pb3.py
@@ -7,4 +7,17 @@ with open("day3/data1.data") as f:
         max_1 = np.argmax([int(c) for c in line[:len(line) - 1]])
         max_2 = max_1 + 1 + np.argmax([int(c) for c in line[max_1 + 1:]])
         result1 += int(str(line[max_1]) + str(line[max_2]))
+
+    f.seek(0)
+    result2 = 0
+    for line in f.readlines():
+        line = line.replace('\n', '')
+        number_str = ''
+        last_max = -1
+        for i in reversed(range(12)):
+            last_max = last_max + 1 + np.argmax([int(c) for c in line[last_max + 1 : len(line) - i]])
+            number_str += str(line[last_max])
+        result2 += int(number_str)
+
     print(result1)
+    print(result2)
