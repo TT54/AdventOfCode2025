cdaab50bb6bd03ce7c7a289a1fc4d0af198cebdf
TT54 <tt54.of@gmail.com>
2025-12-04 07:39:49 +0100

    Résolution problème 2 jour 4

--- day4/pb4.py
+++ day4/pb4.py
@@ -1,3 +1,5 @@
+from copy import deepcopy
+
 def count_paper_around(diagram, i, j):
     count = 0
     for (k, l) in [(u, v) for u in range(-1, 2) for v in range(-1, 2) if (u, v) != (0, 0)]:
@@ -12,5 +14,19 @@ with open('day4/data1.data') as f:
         for j in range(len(diagram[i])):
             if diagram[i][j] == '@' and count_paper_around(diagram, i, j) < 4:
                 result1 += 1
+    
+    result2 = 0
+    removed = 1
+    while(removed > 0):
+        removed = 0
+        new_diagram = deepcopy(diagram)
+        for i in range(len(diagram)):
+            for j in range(len(diagram[i])):
+                if diagram[i][j] == '@' and count_paper_around(diagram, i, j) < 4:
+                    removed += 1
+                    new_diagram[i][j] = '.'
+        diagram = new_diagram
+        result2 += removed
 
     print(f"Result 1 : {result1}")
+    print(f"Result 2 : {result2}")
