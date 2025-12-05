98bf6affdad50c3f5331f1d7ccca40d755282611
TT54 <tt54.of@gmail.com>
2025-12-05 14:06:01 +0100

    Résolution problème 2 jour 5

--- day5/pb5.py
+++ day5/pb5.py
@@ -1,3 +1,41 @@
+def union(begin, end, l:list) -> list:
+    new_list = []
+    begin_inserted = False
+    end_inserted = False
+    for i in range(len(l)):
+        element, is_begin = l[i]
+        if element < begin:                             # Cas < begin
+            new_list.append((element, is_begin))
+        elif element == begin:                          # Cas = begin
+            begin_inserted = True
+            if is_begin: new_list.append((begin, True))
+        else:                                           # Cas > begin
+            if not begin_inserted:
+                begin_inserted = True
+                if is_begin: new_list.append((begin, True))
+            
+            if element < end:                           # Cas < end
+                # On ne fait rien
+                ...
+            elif element == end:                        # Cas = end
+                end_inserted = True
+                if not is_begin: new_list.append((end, False))
+            else:                                       # Cas > end
+                if not end_inserted:
+                    end_inserted = True
+                    if is_begin: new_list.append((end, False))
+                
+                new_list.append((element, is_begin))
+
+    if not begin_inserted:
+        new_list.append((begin, True))
+    
+    if not end_inserted:
+        new_list.append((end, False))
+
+    return new_list
+    
+
 with open("day5/data1.data") as f:
     fresh = []
     result1 = 0
@@ -15,5 +53,14 @@ with open("day5/data1.data") as f:
                 if begin <= value <= end:
                     result1 += 1
                     break
-    
+
+    fresh_list = []
+    for begin, end in fresh:
+        fresh_list = union(begin, end, fresh_list)
+
+    result2 = 0
+    for i in range(0, len(fresh_list), 2):
+        result2 += fresh_list[i + 1][0] - fresh_list[i][0] + 1
+                
     print(f'Result1 : {result1}')
+    print(f'Result2 : {result2}')
