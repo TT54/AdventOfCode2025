de9fb5867efe0106c4d2b6ce73be1e4fb79273b1
TT54 <tt54.of@gmail.com>
2025-12-06 09:33:47 +0100

    Résolution problème 2 jour 6

--- day6/pb6.py
+++ day6/pb6.py
@@ -2,8 +2,10 @@ import re
 
 with open('day6/data1.data') as f:
     problems = []
+    lines = []
     for line in f.readlines():
         problems.append([s for s in re.split("\\s+", line.replace('\n', '')) if len(s) > 0])
+        lines.append(line.replace('\n', ''))
     
     result1 = 0
     for i in range(len(problems[0])):
@@ -12,4 +14,27 @@ with open('day6/data1.data') as f:
             exec("score " + problems[-1][i] + "= " + problems[j][i])
         result1 += score
     
+    result2 = 0
+    cursor = len(lines[0])
+    numbers = []
+    while cursor > 0:
+        cursor -= 1
+        number = ''
+        for i in range(len(lines) - 1):
+            number = number + lines[i][cursor] if lines[i][cursor] != ' ' else number
+        
+        if number == '':
+            continue
+
+        numbers.append(number)
+
+        if lines[-1][cursor] != ' ':
+            score = int(numbers[0])
+            for i in range(1, len(numbers)):
+                exec("score " + lines[-1][cursor] + '= ' + numbers[i])
+            result2 += score
+            numbers.clear()
+
+    
     print(f"Result 1 : {result1}")
+    print(f"Result 2 : {result2}")
