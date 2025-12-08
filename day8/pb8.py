d9ec0c132ce86898f5bc1c1bdc78dbf3ff3d30c9
TT54 <tt54.of@gmail.com>
2025-12-08 07:41:17 +0100

    Résolution problème 2 jour 8

--- day8/pb8.py
+++ day8/pb8.py
@@ -1,4 +1,5 @@
 import numpy as np
+import time
 
 NUMBER_OF_PAIRS = 1000
 
@@ -14,39 +15,53 @@ def calculate_distances(boxes):
             distances[j][i] = distances[i][j]
     return distances
 
+def connect(i, j, array: np.array, elements_group: dict, groups: dict[int: set]):
+    array[i][j] = 1e20
+    array[j][i] = 1e20
+    if i in elements_group.keys() and j in elements_group.keys():
+        if elements_group[i] == elements_group[j]: return
+        old_value = elements_group[j]
+        groups[elements_group[i]] = groups[elements_group[i]].union(groups[old_value])
+        for key in groups[old_value]:
+            if elements_group[key] == old_value: 
+                elements_group[key] = elements_group[i]
+        groups.pop(old_value)
+    elif i in elements_group.keys():
+        elements_group[j] = elements_group[i]
+        groups[elements_group[i]].add(j)
+    elif j in elements_group.keys():
+        elements_group[i] = elements_group[j]
+        groups[elements_group[j]].add(i)
+    else:
+        elements_group[i] = len(elements_group)
+        elements_group[j] = elements_group[i]
+        groups[elements_group[i]] = set([i, j])
+
 with open('day8/data1.data') as f:
+    begin = time.time()
     boxes = []
     for line in f.readlines():
         boxes.append((int(line.split(',')[0]), int(line.split(',')[1]), int(line.split(',')[2])))
     array = np.array(calculate_distances(boxes))
     groups = {}
+    elements_group = {}
     for _ in range(NUMBER_OF_PAIRS):
         argmax = np.argmin(array)
         (i, j) = (argmax // len(array), argmax % len(array))
-        array[i][j] = 1e20
-        array[j][i] = 1e20
-        if i in groups.keys() and j in groups.keys():
-            if groups[i] == groups[j]: continue
-            old_value = groups[j]
-            for key in groups.keys():
-                if groups[key] == old_value: 
-                    groups[key] = groups[i]
-        elif i in groups.keys():
-            groups[j] = groups[i]
-        elif j in groups.keys():
-            groups[i] = groups[j]
-        else:
-            groups[i] = len(groups)
-            groups[j] = groups[i]
+        connect(i, j, array, elements_group, groups)
 
-    results = {}
-    for key in groups.keys():
-        if groups[key] in results: results[groups[key]] += 1
-        else: results[groups[key]] = 1
-
-    results = list(results.values())
+    results = [len(s) for s in groups.values()]
     results.sort()
-    
+
     result1 = results[-1] * results[-2] * results[-3]
 
+    result2 = 0
+    while(len(list(groups.values())[0]) != len(boxes)):
+        argmax = np.argmin(array)
+        (i, j) = (argmax // len(array), argmax % len(array))
+        connect(i, j, array, elements_group, groups)
+        result2 = boxes[i][0] * boxes[j][0]
+
     print(f"Result 1 : {result1}")
+    print(f"Result 2 : {result2}")
+    print(time.time() - begin)
